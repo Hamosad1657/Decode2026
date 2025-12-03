252b60e39f898c98bb7bec2898e275dea85ac1d4
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2025-12-03 19:32:42 +0200

    Added directions and basis for loader subsystem

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/loader/LoaderConstants.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/loader/LoaderConstants.kt
@@ -1,14 +1,25 @@
 package org.firstinspires.ftc.teamcode.subsystems.loader
 
+import com.hamosad.lib.math.PIDGains
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.Servo
+
 object LoaderConstants {
     // NAMES
-    const val SPIN_SERVO_NAME = "Loader spin servo"
+    const val ROULETTE_SERVO_NAME = "Loader spin servo"
     const val COLOR_SENSOR_NAME = "Loader color sensor"
     const val ARM_SERVO_NAME = "Loader arm servo"
     const val ARM_MOTOR_NAME = "Loader arm motor"
 
     // COMPONENT CONFIGS
-
+    val ROULETTE_SERVO_DIRECTION = DcMotorSimple.Direction.FORWARD
+    val ARM_SERVO_DIRECTION = Servo.Direction.FORWARD
+    val ARM_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD
 
     // PID GAINS
+    val ROULETTE_GAINS = PIDGains(
+        0.0,
+        0.0,
+        0.0,
+    )
 }
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/loader/LoaderSubsystem.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/loader/LoaderSubsystem.kt
@@ -5,19 +5,43 @@ import com.hamosad.lib.commands.Subsystem
 import com.hamosad.lib.components.motors.HaCRServoMotor
 import com.hamosad.lib.components.motors.HaMotor
 import com.hamosad.lib.components.motors.HaServoMotor
+import com.hamosad.lib.components.motors.MotorType
 import com.hamosad.lib.components.sensors.HaColorSensor
+import com.hamosad.lib.math.PIDController
+import com.hamosad.lib.math.Rotation2d
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.firstinspires.ftc.teamcode.subsystems.loader.LoaderConstants as Constants
 
 object LoaderSubsystem: Subsystem() {
-    var spinServo: HaCRServoMotor? = null
-    var colorSensor: HaColorSensor? = null
+    private var rouletteServo: HaCRServoMotor? = null
+    private val rouletteController = PIDController(Constants.ROULETTE_GAINS)
 
-    var armServo: HaServoMotor? = null
-    var armMotor: HaMotor? = null
+    private var colorSensor: HaColorSensor? = null
+
+    private var armServo: HaServoMotor? = null
+    private var armMotor: HaMotor? = null
 
     override fun init(newHardwareMap: HardwareMap) {
         super.init(newHardwareMap)
+        rouletteServo = HaCRServoMotor(Constants.ROULETTE_SERVO_NAME, hardwareMap!!)
+        colorSensor = HaColorSensor(Constants.COLOR_SENSOR_NAME, hardwareMap!!)
+        armServo = HaServoMotor(Constants.ARM_SERVO_NAME, hardwareMap!!, Rotation2d.fromDegrees(180.0))
+        armMotor = HaMotor(Constants.ARM_MOTOR_NAME, hardwareMap!!, MotorType.REV_THROUGH_BORE_ENCODER)
+
+        rouletteServo?.direction = Constants.ROULETTE_SERVO_DIRECTION
+        armServo?.direction = Constants.ARM_SERVO_DIRECTION
+        armMotor?.direction = Constants.ARM_MOTOR_DIRECTION
+    }
+    // Property getters
+    private val rouletteAngle: Rotation2d
+        get() = armMotor?.currentPosition ?: Rotation2d.fromDegrees(0.0) // Encoder is connected to the arm motor's encoder port
+
+    // Roulette
+    private var angleSetpoint = Rotation2d.fromDegrees(0.0)
+    private fun updateRouletteControl(newSetpoint: Rotation2d = angleSetpoint) {
+        angleSetpoint = newSetpoint
+        rouletteServo?.setVoltage(rouletteController.calculate(rouletteAngle.asRadians, angleSetpoint.asRadians))
     }
 
     // Periodic
