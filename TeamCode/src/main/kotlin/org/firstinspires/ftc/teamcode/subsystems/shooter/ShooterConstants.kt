80c1d52edd16d072daea694fb0115745b66a592f
Alon Gal <hamosadr@gmail.com>
2025-12-04 18:46:40 +0200

    added angle to the shooter subsystem

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/shooter/ShooterConstants.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/shooter/ShooterConstants.kt
@@ -1,17 +1,34 @@
 package org.firstinspires.ftc.teamcode.subsystems.shooter
 
 import com.hamosad.lib.math.PIDGains
+import com.hamosad.lib.math.Rotation2d
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.Servo
 
 object ShooterConstants {
-    val WHEEL_GAINS = PIDGains(
+    val WHEEL_VELOCITY_GAINS = PIDGains(
         0.0,
         0.0,
         0.0
     )
 
-    const val TRANSMISSION_RATIO: Double = 0.0
+    val HOOD_ANGLE_GAINS = PIDGains(
+        0.0,
+        0.0,
+        0.0
+    )
+
+
+    val LEFT_MOTOR_NAME = ""
+    val RIGHT_MOTOR_NAME = ""
+    val SERVO_NAME = ""
+    const val SPEED_TRANSMISSION_RATIO: Double = 0.0
+    const val HOOD_ANGLE_TRANSMISSION_RATIO: Double = 0.0
     val LEFT_MOTOR_DIRECTION: DcMotorSimple.Direction = DcMotorSimple.Direction.FORWARD
     val RIGHT_MOTOR_DIRECTION: DcMotorSimple.Direction = DcMotorSimple.Direction.FORWARD
+    val SERVO_DIRECTION = Servo.Direction.FORWARD
+
+    val MAX_HOOD_ANGLE: Rotation2d = Rotation2d.fromDegrees(0.0)
+    val MIN_HOOD_ANGLE: Rotation2d = Rotation2d.fromDegrees(0.0)
 
 }
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/shooter/ShooterSubsystem.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/shooter/ShooterSubsystem.kt
@@ -2,34 +2,45 @@ package org.firstinspires.ftc.teamcode.subsystems.shooter
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket
 import com.hamosad.lib.commands.Subsystem
+import com.hamosad.lib.components.motors.HaCRServoMotor
 import com.hamosad.lib.components.motors.HaMotor
+import com.hamosad.lib.components.motors.HaServoMotor
 import com.hamosad.lib.components.motors.MotorType
 import com.hamosad.lib.math.AngularVelocity
 import com.hamosad.lib.math.PIDController
+import com.hamosad.lib.math.Rotation2d
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
 
 object ShooterSubsystem: Subsystem() {
 
-    val pidController = PIDController(ShooterConstants.WHEEL_GAINS)
+    val speedPIDController = PIDController(ShooterConstants.WHEEL_VELOCITY_GAINS)
     val rightMotor: HaMotor? = null
     val leftMotor: HaMotor? = null
+    val servo: HaCRServoMotor? = null
+    val hoodAnglePIDController = PIDController(ShooterConstants.HOOD_ANGLE_GAINS)
 
     override fun init(newHardwareMap: HardwareMap) {
         super.init(newHardwareMap)
-        val rightMotor = HaMotor("Right Shooter Motor", hardwareMap!!, MotorType.GO_BUILDA5202)
-        val leftMotor = HaMotor("Left Shooter Motor", hardwareMap!!, MotorType.GO_BUILDA5202)
+        val rightMotor = HaMotor(ShooterConstants.RIGHT_MOTOR_NAME, hardwareMap!!, MotorType.GO_BUILDA5202)
+        val leftMotor = HaMotor(ShooterConstants.LEFT_MOTOR_NAME, hardwareMap!!, MotorType.GO_BUILDA5202)
+        val servo = HaServoMotor(ShooterConstants.SERVO_NAME, hardwareMap!!)
         leftMotor.setDirection(ShooterConstants.LEFT_MOTOR_DIRECTION)
         rightMotor.setDirection(ShooterConstants.RIGHT_MOTOR_DIRECTION)
+        servo.direction = ShooterConstants.SERVO_DIRECTION
+        leftMotor.resetEncoder() // shooter must start at min angle
     }
 
     val currentMotorVelocity: AngularVelocity get() = rightMotor?.currentVelocity ?: AngularVelocity.fromRPM(0.0)
-    val currentWheelVelocity: AngularVelocity get() = rightMotor?.currentVelocity?.times(
-        ShooterConstants.TRANSMISSION_RATIO) ?: AngularVelocity.fromRPM(0.0)
+    val currentServoAngle: Rotation2d get() = leftMotor?.currentPosition ?: Rotation2d.fromDegrees(0.0) //we are using the left motor's encoder slot for the servo's encoder
+    val currentHoodAngle: Rotation2d get() = ShooterConstants.MIN_HOOD_ANGLE + currentServoAngle * ShooterConstants.HOOD_ANGLE_TRANSMISSION_RATIO
 
-    fun setWheelVelocity(desiredVelocity: AngularVelocity) {
-        rightMotor?.setVoltage(pidController.calculate(currentWheelVelocity.asRPS, desiredVelocity.asRPS / ShooterConstants.TRANSMISSION_RATIO))
-        leftMotor?.setVoltage(pidController.calculate(currentWheelVelocity.asRPS, desiredVelocity.asRPS / ShooterConstants.TRANSMISSION_RATIO))
+    val currentShooterVelocity: AngularVelocity get() = rightMotor?.currentVelocity?.times(
+        ShooterConstants.SPEED_TRANSMISSION_RATIO) ?: AngularVelocity.fromRPM(0.0)
+
+    fun updateShooterVelocityControl(desiredVelocity: AngularVelocity) {
+        rightMotor?.setVoltage(speedPIDController.calculate(currentShooterVelocity.asRPS, desiredVelocity.asRPS / ShooterConstants.SPEED_TRANSMISSION_RATIO))
+        leftMotor?.setVoltage(speedPIDController.calculate(currentShooterVelocity.asRPS, desiredVelocity.asRPS / ShooterConstants.SPEED_TRANSMISSION_RATIO))
     }
 
     fun stopWheel() {
@@ -37,8 +48,13 @@ object ShooterSubsystem: Subsystem() {
         leftMotor?.stopMotor()
     }
 
-    override fun periodic() {
+    fun setHoodAngle(desiredAngle: Rotation2d) {
+        if (desiredAngle.asDegrees in ShooterConstants.MIN_HOOD_ANGLE.asDegrees..ShooterConstants.MAX_HOOD_ANGLE.asDegrees) {
+            servo?.setVoltage(hoodAnglePIDController.calculate(currentHoodAngle.asDegrees, desiredAngle.asDegrees))
+        }
+    }
 
+    override fun periodic() {
     }
 
     override fun updateTelemetry(telemetry: Telemetry, dashboardPacket: TelemetryPacket) {
