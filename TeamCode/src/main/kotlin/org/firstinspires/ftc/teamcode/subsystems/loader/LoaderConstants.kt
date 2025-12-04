95af6e3681a393321b27cea823d06d46595521c7
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2025-12-04 19:01:09 +0200

    Finished working on loader subsystem

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/loader/LoaderConstants.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/loader/LoaderConstants.kt
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.subsystems.loader
 
+import com.hamosad.lib.components.sensors.Color
 import com.hamosad.lib.math.PIDGains
+import com.hamosad.lib.math.Rotation2d
+import com.hamosad.lib.math.Volts
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.Servo
 
@@ -16,10 +19,46 @@ object LoaderConstants {
     val ARM_SERVO_DIRECTION = Servo.Direction.FORWARD
     val ARM_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD
 
+    val ARM_SERVO_RANGE = Rotation2d.fromDegrees(180.0)
+
     // PID GAINS
-    val ROULETTE_GAINS = PIDGains(
+    val ROULETTE_ANGLE_GAINS = PIDGains(
         0.0,
         0.0,
         0.0,
     )
+
+    // ROULETTE VALUES (Counter clockwise increasing)
+    val BALL_1_AT_SHOOTER = Rotation2d.fromDegrees(0.0)
+    val BALL_1_AT_INTAKE = BALL_1_AT_SHOOTER + Rotation2d.fromDegrees(90.0)
+    val BALL_1_AT_COLOR_SENSOR = BALL_1_AT_SHOOTER - Rotation2d.fromDegrees(45.0)
+
+    val BALL_2_AT_SHOOTER = Rotation2d.fromDegrees(120.0)
+    val BALL_2_AT_INTAKE = BALL_2_AT_SHOOTER + Rotation2d.fromDegrees(90.0)
+    val BALL_2_AT_COLOR_SENSOR = BALL_2_AT_SHOOTER - Rotation2d.fromDegrees(45.0)
+
+
+    val BALL_3_AT_SHOOTER = Rotation2d.fromDegrees(240.0)
+    val BALL_3_AT_INTAKE = BALL_3_AT_SHOOTER + Rotation2d.fromDegrees(90.0)
+    val BALL_3_AT_COLOR_SENSOR = BALL_3_AT_SHOOTER - Rotation2d.fromDegrees(45.0)
+
+    val ROULETTE_TOLERANCE = Rotation2d.fromDegrees(4.0)
+
+    val COLOR_SENSOR_ANGLE_TOLERANCE = Rotation2d.fromDegrees(4.0)
+    const val COLOR_SENSOR_TOLERANCE: Int = 30
+    val GREEN_COLOR = Color(0, 255, 0)
+    val PURPLE_COLOR = Color(255, 0, 255)
+
+    // ARM VALUES
+    val RETRACTED_ARM_ANGLE = Rotation2d.fromDegrees(0.0)
+    val OPEN_ARM_ANGLE = Rotation2d.fromDegrees(0.0)
+
+    const val ARM_MOTOR_VOLTAGE: Volts = 6.0
+
+}
+
+enum class BallColor {
+    PURPLE,
+    GREEN,
+    UNKNOWN,
 }
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/loader/LoaderSubsystem.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/loader/LoaderSubsystem.kt
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.teamcode.subsystems.loader.LoaderConstants as Const
 
 object LoaderSubsystem: Subsystem() {
     private var rouletteServo: HaCRServoMotor? = null
-    private val rouletteController = PIDController(Constants.ROULETTE_GAINS)
+    private val rouletteController = PIDController(Constants.ROULETTE_ANGLE_GAINS)
 
     private var colorSensor: HaColorSensor? = null
 
@@ -26,31 +26,85 @@ object LoaderSubsystem: Subsystem() {
         super.init(newHardwareMap)
         rouletteServo = HaCRServoMotor(Constants.ROULETTE_SERVO_NAME, hardwareMap!!)
         colorSensor = HaColorSensor(Constants.COLOR_SENSOR_NAME, hardwareMap!!)
-        armServo = HaServoMotor(Constants.ARM_SERVO_NAME, hardwareMap!!, Rotation2d.fromDegrees(180.0))
+        armServo = HaServoMotor(Constants.ARM_SERVO_NAME, hardwareMap!!, Constants.ARM_SERVO_RANGE)
         armMotor = HaMotor(Constants.ARM_MOTOR_NAME, hardwareMap!!, MotorType.REV_THROUGH_BORE_ENCODER)
 
         rouletteServo?.direction = Constants.ROULETTE_SERVO_DIRECTION
         armServo?.direction = Constants.ARM_SERVO_DIRECTION
         armMotor?.direction = Constants.ARM_MOTOR_DIRECTION
     }
-    // Property getters
+    // Properties
     private val rouletteAngle: Rotation2d
         get() = armMotor?.currentPosition ?: Rotation2d.fromDegrees(0.0) // Encoder is connected to the arm motor's encoder port
 
-    // Roulette
+    val isAtSetpoint: Boolean
+        get() = (rouletteAngle.asDegrees in (
+                angleSetpoint.asDegrees - Constants.ROULETTE_TOLERANCE.asDegrees)..(angleSetpoint.asDegrees + Constants.ROULETTE_TOLERANCE.asDegrees))
+
+    var ball1Color: BallColor = BallColor.UNKNOWN
+        private set
+    var ball2Color: BallColor = BallColor.UNKNOWN
+        private set
+    var ball3Color: BallColor = BallColor.UNKNOWN
+        private set
+
+    // Roulette functions
     private var angleSetpoint = Rotation2d.fromDegrees(0.0)
-    private fun updateRouletteControl(newSetpoint: Rotation2d = angleSetpoint) {
+    fun updateRouletteControl(newSetpoint: Rotation2d = angleSetpoint) {
+        // TODO: Add closer side approximation
         angleSetpoint = newSetpoint
         rouletteServo?.setVoltage(rouletteController.calculate(rouletteAngle.asRadians, angleSetpoint.asRadians))
     }
 
+    private fun determineCurrentColor(): BallColor {
+        if (colorSensor?.isInColorRange(Constants.GREEN_COLOR, Constants.COLOR_SENSOR_TOLERANCE) ?: false) {
+            return BallColor.GREEN
+        } else if (colorSensor?.isInColorRange(Constants.PURPLE_COLOR, Constants.COLOR_SENSOR_TOLERANCE) ?: false) {
+            return BallColor.PURPLE
+        } else {
+            return BallColor.UNKNOWN
+        }
+    }
+
+    private fun updateBallColors() {
+        when (rouletteAngle.asDegrees) {
+            in (Constants.BALL_1_AT_COLOR_SENSOR.asDegrees - Constants.COLOR_SENSOR_ANGLE_TOLERANCE.asDegrees)..(
+                    Constants.BALL_1_AT_COLOR_SENSOR.asDegrees + Constants.COLOR_SENSOR_ANGLE_TOLERANCE.asDegrees) -> {
+                ball1Color = determineCurrentColor()
+            }
+
+            in (Constants.BALL_2_AT_COLOR_SENSOR.asDegrees - Constants.COLOR_SENSOR_ANGLE_TOLERANCE.asDegrees)..(
+                    Constants.BALL_2_AT_COLOR_SENSOR.asDegrees + Constants.COLOR_SENSOR_ANGLE_TOLERANCE.asDegrees) -> {
+                ball2Color = determineCurrentColor()
+            }
+
+            in (Constants.BALL_3_AT_COLOR_SENSOR.asDegrees - Constants.COLOR_SENSOR_ANGLE_TOLERANCE.asDegrees)..(
+                    Constants.BALL_3_AT_COLOR_SENSOR.asDegrees + Constants.COLOR_SENSOR_ANGLE_TOLERANCE.asDegrees) -> {
+                ball3Color = determineCurrentColor()
+            }
+        }
+    }
+
+    // Arm functions
+    fun loadToShooter() {
+        armServo?.currentCommandedPosition = Constants.OPEN_ARM_ANGLE
+        armMotor?.setVoltage(Constants.ARM_MOTOR_VOLTAGE)
+    }
+
+    fun stopLoadingFromShooter() {
+        armServo?.currentCommandedPosition = Constants.RETRACTED_ARM_ANGLE
+        armMotor?.stopMotor()
+    }
+
     // Periodic
     override fun periodic() {
-
+        updateBallColors()
     }
 
     // Telemetry
     override fun updateTelemetry(telemetry: Telemetry, dashboardPacket: TelemetryPacket) {
-
+        telemetry.addData("Ball 1 color", ball1Color.name)
+        telemetry.addData("Ball 2 color", ball2Color.name)
+        telemetry.addData("Ball 3 color", ball3Color.name)
     }
 }
