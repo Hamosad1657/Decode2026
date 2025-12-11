6ca4a5f65f7714feca358c60df4102018b3b4474
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2025-12-11 15:05:37 +0200

    Added Commands to do

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/LoaderCommands.kt
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.commands
+
+/*
+    Commands needed:
+    General:
+    - Hold roulette position
+
+    Shooting:
+    - Position ball to shoot
+    - Load to shooter
+    - Position and load to shooter
+
+    Intaking:
+    - Position ball to intake
+ */
+
