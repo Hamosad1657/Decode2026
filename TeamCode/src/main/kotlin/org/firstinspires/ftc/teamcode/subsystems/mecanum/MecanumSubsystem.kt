ac3173f9b23a7749af3c05b7a8119e6620d19a3d
Alon Gal <hamosadr@gmail.com>
2025-12-11 17:06:28 +0200

    tried haColorCamera and tried to fix it

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/mecanum/MecanumSubsystem.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystems/mecanum/MecanumSubsystem.kt
@@ -14,10 +14,14 @@ import com.hamosad.lib.math.Translation2d
 import com.hamosad.lib.math.Translation3d
 import com.hamosad.lib.vision.AprilTagsStdDevs
 import com.hamosad.lib.vision.HaAprilTagCamera
+import com.hamosad.lib.vision.HaColorCamera
 import com.hamosad.lib.vision.RobotPoseStdDevs
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor
+import org.firstinspires.ftc.vision.opencv.ColorRange
+import org.firstinspires.ftc.vision.opencv.ColorSpace
 import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumConstants as Constants
 import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumKinematics as Kinematics
 
@@ -33,7 +37,7 @@ object MecanumSubsystem: Subsystem() {
     private var imu: HaIMU? = null
 
     var USE_VISION = false
-    var aprilTagCamera: HaAprilTagCamera? = null
+    var blobCamera: HaColorCamera? = null
 
     override fun init(newHardwareMap: HardwareMap) {
         super.init(newHardwareMap)
@@ -51,21 +55,20 @@ object MecanumSubsystem: Subsystem() {
         imu = HaIMU("IMU", hardwareMap!!)
 
         if (USE_VISION) {
-            aprilTagCamera = HaAprilTagCamera(
+            blobCamera = HaColorCamera(
                 hardwareMap!!,
-                "camera",
-                Length.fromMeters(5.0),
-                7.0,
-                Translation3d(0.0, 0.0, 0.18),
-                Rotation3d(
-                    Rotation2d.fromDegrees(45.0),
-                    Rotation2d.fromDegrees(0.0),
-                    Rotation2d.fromDegrees(0.0)
-                ),
-                AprilTagsStdDevs(
-                    RobotPoseStdDevs(0.0, 0.0, 0.0),
-                    RobotPoseStdDevs(0.0 ,0.0 ,0.0)
-                )
+                "Webcam 1",
+                0,
+                ColorRange(
+                    ColorSpace.RGB,
+                    org.opencv.core.Scalar(30.0, 120.0, 150.0),
+                    org.opencv.core.Scalar(60.0, 170.0, 255.0)),
+                5,
+                5,
+                5,
+                ColorBlobLocatorProcessor.BlobCriteria.BY_CONTOUR_AREA,
+                150.0,
+                10000.0,
             )
         }
     }
@@ -127,35 +130,33 @@ object MecanumSubsystem: Subsystem() {
     // Telemetry
     override fun updateTelemetry(telemetry: Telemetry, dashboardPacket: TelemetryPacket) {
 
-        telemetry.addData("Angle deg", currentAngle.asDegrees)
-        telemetry.addData("Requested chassis speeds angle deg", requestedChassisSpeedsTranslation.rotation.asDegrees)
-
-        // FL, BR, FR, BL
-        if(wheelVelocitySetpoints.lastIndex == 3) {
-        telemetry.addData("Commanded velocity FL RPM", wheelVelocitySetpoints[0].asRPM)
-        telemetry.addData("Commanded velocity BR RPM", wheelVelocitySetpoints[1].asRPM)
-        telemetry.addData("Commanded velocity FR RPM", wheelVelocitySetpoints[2].asRPM)
-        telemetry.addData("Commanded velocity BL RPM", wheelVelocitySetpoints[3].asRPM)
-        } else {
-            telemetry.addData("Commanded velocity FL RPM", 0.0)
-            telemetry.addData("Commanded velocity BR RPM", 0.0)
-            telemetry.addData("Commanded velocity FR RPM", 0.0)
-            telemetry.addData("Commanded velocity BL RPM", 0.0)
-        }
-        // telemetry.addData("Pose X", aprilTagCamera!!.estimatedPose?.translation2d?.x)
-        // telemetry.addData("Pose Y", aprilTagCamera!!.estimatedPose?.translation2d?.y)
-        // telemetry.addData("Pose Rotation", aprilTagCamera!!.estimatedPose?.rotation2d?.asDegrees)
-
-        if (wheelVelocitySetpoints.size == 4) {
-            dashboardPacket.put("FL setpoint RPM", wheelVelocitySetpoints[0].asRPM)
-            dashboardPacket.put("BR setpoint RPM", wheelVelocitySetpoints[1].asRPM)
-            dashboardPacket.put("FR setpoint RPM", wheelVelocitySetpoints[2].asRPM)
-            dashboardPacket.put("BL setpoint RPM", wheelVelocitySetpoints[3].asRPM)
-
-            dashboardPacket.put("FL velocity RPM", motors[0].currentVelocity.asRPM)
-            dashboardPacket.put("BR velocity RPM", motors[1].currentVelocity.asRPM)
-            dashboardPacket.put("FR velocity RPM", motors[2].currentVelocity.asRPM)
-            dashboardPacket.put("BL velocity RPM", motors[3].currentVelocity.asRPM)
-        }
+        telemetry.addData("has targets", blobCamera?.bestBlob != null)
+//        telemetry.addData("Angle deg", currentAngle.asDegrees)
+//        telemetry.addData("Requested chassis speeds angle deg", requestedChassisSpeedsTranslation.rotation.asDegrees)
+//
+//        // FL, BR, FR, BL
+//        if(wheelVelocitySetpoints.lastIndex == 3) {
+//        telemetry.addData("Commanded velocity FL RPM", wheelVelocitySetpoints[0].asRPM)
+//        telemetry.addData("Commanded velocity BR RPM", wheelVelocitySetpoints[1].asRPM)
+//        telemetry.addData("Commanded velocity FR RPM", wheelVelocitySetpoints[2].asRPM)
+//        telemetry.addData("Commanded velocity BL RPM", wheelVelocitySetpoints[3].asRPM)
+//        } else {
+//            telemetry.addData("Commanded velocity FL RPM", 0.0)
+//            telemetry.addData("Commanded velocity BR RPM", 0.0)
+//            telemetry.addData("Commanded velocity FR RPM", 0.0)
+//            telemetry.addData("Commanded velocity BL RPM", 0.0)
+//        }
+//
+//        if (wheelVelocitySetpoints.size == 4) {
+//            dashboardPacket.put("FL setpoint RPM", wheelVelocitySetpoints[0].asRPM)
+//            dashboardPacket.put("BR setpoint RPM", wheelVelocitySetpoints[1].asRPM)
+//            dashboardPacket.put("FR setpoint RPM", wheelVelocitySetpoints[2].asRPM)
+//            dashboardPacket.put("BL setpoint RPM", wheelVelocitySetpoints[3].asRPM)
+//
+//            dashboardPacket.put("FL velocity RPM", motors[0].currentVelocity.asRPM)
+//            dashboardPacket.put("BR velocity RPM", motors[1].currentVelocity.asRPM)
+//            dashboardPacket.put("FR velocity RPM", motors[2].currentVelocity.asRPM)
+//            dashboardPacket.put("BL velocity RPM", motors[3].currentVelocity.asRPM)
+//        }
     }
 }
