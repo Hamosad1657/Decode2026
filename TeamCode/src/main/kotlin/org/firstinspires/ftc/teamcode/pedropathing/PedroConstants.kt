aa4bac1b4229a4818dc5f70c3716cf2db91becec
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2025-12-14 19:29:27 +0200

    Messed with pedro pathing

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/pedropathing/PedroConstants.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/pedropathing/PedroConstants.kt
@@ -3,18 +3,56 @@ package org.firstinspires.ftc.teamcode.pedropathing
 import com.pedropathing.follower.Follower
 import com.pedropathing.follower.FollowerConstants
 import com.pedropathing.ftc.FollowerBuilder
+import com.pedropathing.ftc.drivetrains.MecanumConstants
+import com.pedropathing.ftc.localization.Encoder
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants
 import com.pedropathing.paths.PathConstraints
+import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
 
 object PedroConstants {
-    var followerConstants: FollowerConstants = FollowerConstants().mass(5.0)
+    // Technical constants
+    val driveConstants: MecanumConstants = MecanumConstants().apply {
+        maxPower(1.0)
+        leftFrontMotorName ="FL"
+        rightRearMotorName = "BR"
+        rightFrontMotorName = "FR"
+        leftRearMotorName = "BL"
+        leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE
+        rightRearMotorDirection = DcMotorSimple.Direction.FORWARD
+        rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD
+        leftRearMotorDirection = DcMotorSimple.Direction.REVERSE
+    }
+
+    val localizerConstants: DriveEncoderConstants = DriveEncoderConstants().apply {
+        leftFrontMotorName = "FL"
+        rightRearMotorName = "BR"
+        rightFrontMotorName = "FR"
+        leftRearMotorName = "BL"
+        leftFrontEncoderDirection = Encoder.REVERSE
+        rightRearEncoderDirection = Encoder.REVERSE
+        rightFrontEncoderDirection = Encoder.REVERSE
+        leftRearEncoderDirection = Encoder.REVERSE
+
+        robot_Length = 13.188
+        robot_Width = 16.535
+
+        forwardTicksToInches = -5.449
+    }
+
+    // Following tuning specific constants
+    var followerConstants: FollowerConstants = FollowerConstants().apply {
+        mass = 5.0
+    }
 
     var pathConstraints: PathConstraints = PathConstraints(0.99, 100.0, 1.0, 1.0)
 
     @JvmStatic
     fun createFollower(hardwareMap: HardwareMap): Follower {
-        return FollowerBuilder(followerConstants, hardwareMap)
-            .pathConstraints(pathConstraints)
-            .build()
+        return FollowerBuilder(followerConstants, hardwareMap).apply {
+            driveEncoderLocalizer(localizerConstants)
+            pathConstraints(pathConstraints)
+            mecanumDrivetrain(driveConstants)
+        }.build()
     }
 }
