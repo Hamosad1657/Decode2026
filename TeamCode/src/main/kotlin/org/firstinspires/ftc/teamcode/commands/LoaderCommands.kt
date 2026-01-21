352fd454dfb4b7c7f7ce8f7134ea50f25f3b4b84
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2026-01-21 19:14:25 +0200

    Added lambda command variants to LoaderCommands, and used them to fix positionColorToShooterCommand

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/LoaderCommands.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/commands/LoaderCommands.kt
@@ -18,26 +18,14 @@ enum class Ball {
     BALL_3,
 }
 
-/*
-    Commands needed:
-    General:
-    - Hold roulette position
-
-    Shooting:
-    - Position ball to shoot
-    - Load to shooter
-    - Position and load to shooter
-    - Shoot green ball
-    - Shoot purple ball
-    - Shoot closest
-
-    Intaking:
-    - Position ball to intake
- */
-
 // GENERAL
 fun LoaderSubsystem.holdRoulettePositionCommand(position: HaRotation2d): Command = runCommand {
     updateRouletteControl(position)
+    stopLoadingToShooter()
+}
+
+fun LoaderSubsystem.holdRoulettePositionCommand(position: () -> HaRotation2d): Command = runCommand {
+    updateRouletteControl(position())
     stopLoadingToShooter()
 }
 
@@ -50,25 +38,26 @@ fun LoaderSubsystem.positionBallToShooterCommand(ball: Ball): Command = holdRoul
     }
 )
 
+fun LoaderSubsystem.positionBallToShooterCommand(ball: () -> Ball): Command = holdRoulettePositionCommand {
+    when (ball()) {
+        Ball.BALL_1 -> Constants.BALL_1_AT_SHOOTER
+        Ball.BALL_2 -> Constants.BALL_2_AT_SHOOTER
+        Ball.BALL_3 -> Constants.BALL_3_AT_SHOOTER
+    }
+}
+
 fun LoaderSubsystem.loadToShooterCommand(): Command = runCommand {
     updateRouletteControl()
     loadToShooter()
 } finallyDo { stopLoadingToShooter() }
 
 fun LoaderSubsystem.positionColorToShooterCommand(color: BallColor): Command =
-    when (color) {
-        BallColor.UNKNOWN -> runOnce { }
-        returnBallColor(closestBallToShooter) -> {
-            positionBallToShooterCommand(Ball.BALL_1)
-        }
-        returnBallColor(middleBallFromShooter) -> {
-            positionBallToShooterCommand(Ball.BALL_2)
-        }
-        returnBallColor(furthestBallFromShooter) -> {
-            positionBallToShooterCommand(Ball.BALL_3)
-        }
-        else -> {
-            runOnce { }
+    positionBallToShooterCommand {
+        when (color) {
+            returnBallColor(closestBallToShooter) -> closestBallToShooter
+            returnBallColor(middleBallFromShooter) -> middleBallFromShooter
+            returnBallColor(furthestBallFromShooter) -> furthestBallFromShooter
+            else -> Ball.BALL_1
         }
     }
 
