0c0f3fcd8da876a0a40e92eaed324fd253c2f208
Alon Gal <hamosadr@gmail.com>
2026-01-26 17:26:54 +0200

    stopped in the middle so alon could continue

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/simpleOpMode.kt
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode
+
+import com.hamosad.lib.opModes.CommandOpModeTeleop
+import com.hamosad.lib.commands.Subsystem
+import com.hamosad.lib.components.Controllers.HaCommandController
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.commands.angularVelocityDriveCommand
+import org.firstinspires.ftc.teamcode.subsystems.intake.IntakeSubsystem
+import org.firstinspires.ftc.teamcode.subsystems.loader.LoaderSubsystem
+import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumSubsystem
+import org.firstinspires.ftc.teamcode.subsystems.shooter.ShooterSubsystem
+
+@TeleOp
+class simpleOpMode: CommandOpModeTeleop() {
+    override var subsystemsToUse: List<Subsystem> = listOf(
+        MecanumSubsystem, LoaderSubsystem,
+        ShooterSubsystem, IntakeSubsystem
+    )
+    val controller = HaCommandController({ super.gamepad1 }, 0.03, 1)
+
+    override fun configureDefaultCommands() {
+        MecanumSubsystem.defaultCommand = MecanumSubsystem.angularVelocityDriveCommand(
+            true,
+            { controller.getLeftX() },
+            { controller.getLeftY() },
+            { controller.getRightX() },
+        )
+    }
+
+    override fun configureBindings() {
+        controller.r2Pressed()
+    }
+}
