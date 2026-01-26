a8d7bdb88c214ac3bc3edcd45a75cca6bcd66362
Alon Gal <hamosadr@gmail.com>
2026-01-26 08:54:13 +0200

    added shooter interpolation to test op mode

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/testOpModes/ShooterTestOpMode.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/testOpModes/ShooterTestOpMode.kt
@@ -7,12 +7,14 @@ import com.hamosad.lib.commands.Subsystem
 import com.hamosad.lib.components.Controllers.HaCommandController
 import com.hamosad.lib.math.AngularVelocity
 import com.hamosad.lib.math.HaRotation2d
+import com.hamosad.lib.math.Length
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.commands.maintainHoodAngleAndWheelSpeedCommand
 import org.firstinspires.ftc.teamcode.commands.maintainHoodAngleCommand
 import org.firstinspires.ftc.teamcode.commands.maintainWheelSpeedCommand
 import org.firstinspires.ftc.teamcode.commands.setWheelMotorsVoltageCommand
 import org.firstinspires.ftc.teamcode.subsystems.shooter.ShooterState
+import org.firstinspires.ftc.teamcode.subsystems.shooter.interpolateDistanceToShooterState
 
 @TeleOp
 class ShooterTestOpMode: CommandOpModeTeleop() {
@@ -27,7 +29,8 @@ class ShooterTestOpMode: CommandOpModeTeleop() {
         controller.l2Pressed().whileTrue(ShooterSubsystem.setWheelMotorsVoltageCommand(9.0))
         controller.r1().whileTrue(ShooterSubsystem.maintainHoodAngleCommand(HaRotation2d.fromDegrees(50.0)))
         controller.l1().whileTrue(ShooterSubsystem.maintainWheelSpeedCommand(AngularVelocity.fromRPS(50.0)))
-
+        controller.cross().whileTrue(ShooterSubsystem.maintainHoodAngleAndWheelSpeedCommand(interpolateDistanceToShooterState(
+            Length.fromMeters(1.0))))
     }
 
     override fun configureDefaultCommands() {
