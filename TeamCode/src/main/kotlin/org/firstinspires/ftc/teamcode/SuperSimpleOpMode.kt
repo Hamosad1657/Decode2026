919198c703baf00186fd3fd7b23ec96192954cd7
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2026-01-28 06:58:10 +0200

    Added the super simple op mode

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/SuperSimpleOpMode.kt
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode
+
+import com.hamosad.lib.commands.Subsystem
+import com.hamosad.lib.components.Controllers.HaCommandController
+import com.hamosad.lib.math.AngularVelocity
+import com.hamosad.lib.math.HaRotation2d
+import com.hamosad.lib.opModes.CommandOpModeTeleop
+import org.firstinspires.ftc.teamcode.commands.angularVelocityDriveCommand
+import org.firstinspires.ftc.teamcode.commands.disableIntakeCommand
+import org.firstinspires.ftc.teamcode.commands.maintainHoodAngleAndWheelSpeedCommand
+import org.firstinspires.ftc.teamcode.commands.maintainWheelSpeedCommand
+import org.firstinspires.ftc.teamcode.commands.manualControlCommand
+import org.firstinspires.ftc.teamcode.commands.runIntakeCommand
+import org.firstinspires.ftc.teamcode.commands.runIntakeReverseCommand
+import org.firstinspires.ftc.teamcode.subsystems.intake.IntakeSubsystem
+import org.firstinspires.ftc.teamcode.subsystems.loader.LoaderSubsystem
+import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumSubsystem
+import org.firstinspires.ftc.teamcode.subsystems.shooter.ShooterState
+import org.firstinspires.ftc.teamcode.subsystems.shooter.ShooterSubsystem
+
+class SuperSimpleOpMode: CommandOpModeTeleop() {
+    val stationaryShootingState: ShooterState = ShooterState(HaRotation2d.fromDegrees(0.0), AngularVelocity.fromRPM(0.0))
+
+    val controller1 = HaCommandController({ super.gamepad1 }, 0.03, 1)
+    val controller2 = HaCommandController({ super.gamepad2 }, 0.03, 1)
+    override var subsystemsToUse: List<Subsystem> = listOf(
+        MecanumSubsystem, LoaderSubsystem,
+        ShooterSubsystem, IntakeSubsystem
+    )
+
+    override fun configureDefaultCommands() {
+        MecanumSubsystem.defaultCommand = MecanumSubsystem.angularVelocityDriveCommand(
+            true,
+            { controller1.getLeftX() },
+            { controller1.getLeftY() },
+            { controller1.getRightX() },
+        )
+        IntakeSubsystem.defaultCommand = IntakeSubsystem.disableIntakeCommand()
+        LoaderSubsystem.defaultCommand = LoaderSubsystem.manualControlCommand(
+            { controller2.getLeftX() }, { controller2.controller()?.right_bumper ?: false }
+        )
+        ShooterSubsystem.defaultCommand = ShooterSubsystem.maintainHoodAngleAndWheelSpeedCommand(
+            ShooterState(HaRotation2d.fromDegrees(0.0), AngularVelocity.fromRPM(0.0)))
+    }
+
+    override fun configureBindings() {
+        controller1.r2Pressed().whileTrue(IntakeSubsystem.runIntakeCommand())
+        controller1.l2Pressed().whileTrue(IntakeSubsystem.runIntakeReverseCommand())
+
+        controller2.l1().whileTrue(ShooterSubsystem.maintainWheelSpeedCommand(AngularVelocity.fromRPS(90.0)))
+    }
+}
